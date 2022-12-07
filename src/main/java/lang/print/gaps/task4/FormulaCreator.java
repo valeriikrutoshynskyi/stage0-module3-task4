c119dd758e28403df425b893ced8dbfc819eb55e
Valerii Krutoshynskyi <valera9496@gmail.com>
2022-12-07 16:16:19 +0100

    Task 3.4/3

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,6 +4,7 @@ public class FormulaCreator {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
-        System.out.println((9*Math.pow(a, 2) - 5*b + 2 + 2 - 7) * ((a + b - 4*a*b) / 2));
+        double res = (9*Math.pow(a, 2) - 5*b + 2 + a - 7) * ((a + b - 4*a*b) / 2);
+        System.out.println(res);
     }
 }
